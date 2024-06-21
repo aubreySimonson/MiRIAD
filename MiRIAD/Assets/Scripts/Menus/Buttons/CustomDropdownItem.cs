a059cea503cb93307edd6c241b48913de2a7bda2
Aubrey Simonson <followspotfour@gmail.com>
2024-06-21 09:54:55 -0400

    url switcher front end works in VR

--- MiRIAD/Assets/Scripts/Menus/Buttons/CustomDropdownItem.cs
+++ MiRIAD/Assets/Scripts/Menus/Buttons/CustomDropdownItem.cs
@@ -2,11 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
+
 
 public class CustomDropdownItem : MonoBehaviour
 {
+    public Text debugText;
+
     public TMP_Dropdown dropdown;
     public bool autotrigger = false;
+
+    void Start(){
+        debugText = GameObject.Find("Debug").GetComponent<Text>();
+    }
 
     void Update(){
         if(autotrigger){
@@ -20,6 +28,7 @@ public class CustomDropdownItem : MonoBehaviour
 
         //set value to that value
         dropdown.value = gameObject.transform.GetSiblingIndex()-1;
+        dropdown.gameObject.GetComponentInChildren<CustomDropdownToggle>().isOpen = false;
         dropdown.Hide();
     }
 }
--- MiRIAD/Assets/Scripts/Menus/Buttons/CustomDropdownToggle.cs
+++ MiRIAD/Assets/Scripts/Menus/Buttons/CustomDropdownToggle.cs
@@ -2,18 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 
 public class CustomDropdownToggle : MonoBehaviour
 {
     public TMP_Dropdown dropdown;
+    public bool isOpen = false;
+    public Text debugText;
+
+
+
+    void Start(){
+        debugText = GameObject.Find("Debug").GetComponent<Text>();
+    }
+
 
     public void Toggle(){
-        if(dropdown.IsActive()){
-            dropdown.Show();
+        debugText.text = "toggling dropdown. ";
+        if(isOpen){
+            debugText.text += "Dropdown is active. Hiding it now";
+            isOpen = false;
+            dropdown.Hide();
         }
         else{
-            dropdown.Hide();
+            debugText.text += "Dropdown is not active. Showing it now";
+            isOpen = true;
+            dropdown.Show();
         }
     }
 }
